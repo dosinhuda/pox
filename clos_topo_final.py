ff801cdbc59cfa9c146b8b25cc9bbeedd2394809
dosinhuda <dosinhuda@gmail.com>
2017-09-27 07:23:11 +0800

    Add files via upload

--- clos_topo_final.py
+++ clos_topo_final.py
@@ -22,29 +22,29 @@ class ClosTopo(Topo):
        
         "Set up Core and Aggregate level, Connection Core - Aggregation level"
         for c in irange(1, cores):
-            self.addSwitch('cor%s' %c)
+            self.addSwitch('full_node%s' %c)
             for a in irange(1, cores*fanout):
-                self.addSwitch('agg%s' %(a+cores))
-                self.addLink('agg%s' %(a+cores), 'cor%s' %c)
+                self.addSwitch('agw%s' %(a+cores))
+                self.addLink('agw%s' %(a+cores), 'full_node%s' %c)
 
         counter = cores + (cores*fanout) #to deal with mininet numberings
 
         "Set up Edge level, Connection Aggregation - Edge level "
         for a in irange(1, cores*fanout):
             for e in irange(1, cores*(fanout**2)):
-                self.addSwitch('edg%s' %(e+counter))
-                self.addLink('edg%s' %(e+counter), 'agg%s' %(a+cores))
+                self.addSwitch('homegw%s' %(e+counter))
+                self.addLink('homegw%s' %(e+counter), 'agw%s' %(a+cores))
         
         "Set up Host level, Connection Edge - Host level "
         host_pod_offset = 0 #offset per pod accessed by edge switch
         for e in irange(1, cores*(fanout**2)):
             for h in irange(1, fanout):
-                self.addHost('h%s' %(host_pod_offset+h))
-                self.addLink('edg%s' %(e+counter), 'h%s' %(host_pod_offset+h))
+                self.addHost('sta%s' %(host_pod_offset+h))
+                self.addLink('homegw%s' %(e+counter), 'sta%s' %(host_pod_offset+h))
             host_pod_offset = host_pod_offset + fanout
 	
 
-def setup_clos_topo(fanout=2, cores=1):
+def setup_clos_topo(fanout=2, cores=2):
     "Create and test a simple clos network"
     assert(fanout>0)
     assert(cores>0)
@@ -52,7 +52,7 @@ def setup_clos_topo(fanout=2, cores=1):
     net = Mininet(topo=topo, controller=lambda name: RemoteController('c0', "127.0.0.1"), autoSetMacs=True, link=TCLink)
     net.start()
     time.sleep(20) #wait 20 sec for routing to converge
-    net.pingAll()  #test all to all ping and learn the ARP info over this process
+    #net.pingAll()  #test all to all ping and learn the ARP info over this process
     CLI(net)       #invoke the mininet CLI to test your own commands
     net.stop()     #stop the emulation (in practice Ctrl-C from the CLI 
                    #and then sudo mn -c will be performed by programmer)
